19a5643d8015969df0da6935b26b684a5625e9a5
hsteffens <hsteffens@MC-3357.furb.br>
2016-03-29 19:56:52 -0300

    #0 - Criado projeto

---
+++ Compilador/src/analisador/lexico/Constants.java
@@ -0,0 +1,48 @@
+package analisador.lexico;
+public interface Constants extends ScannerConstants {
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_pr = 2;
+    int t_int = 3;
+    int t_float = 4;
+    int t_string = 5;
+    int t_bool = 6;
+    int t_conint = 7;
+    int t_confloat = 8;
+    int t_constring = 9;
+    int t_c_bloco = 10;
+    int t_and = 11;
+    int t_false = 12;
+    int t_if = 13;
+    int t_in = 14;
+    int t_isFalseDo = 15;
+    int t_isTrueDo = 16;
+    int t_main = 17;
+    int t_module = 18;
+    int t_not = 19;
+    int t_or = 20;
+    int t_out = 21;
+    int t_true = 22;
+    int t_while = 23;
+    int t_TOKEN_24 = 24; //","
+    int t_TOKEN_25 = 25; //":"
+    int t_TOKEN_26 = 26; //";"
+    int t_TOKEN_27 = 27; //"["
+    int t_TOKEN_28 = 28; //"]"
+    int t_TOKEN_29 = 29; //"("
+    int t_TOKEN_30 = 30; //")"
+    int t_TOKEN_31 = 31; //"{"
+    int t_TOKEN_32 = 32; //"}"
+    int t_TOKEN_33 = 33; //"-"
+    int t_TOKEN_34 = 34; //"*"
+    int t_TOKEN_35 = 35; //"/"
+    int t_TOKEN_36 = 36; //"<-"
+    int t_TOKEN_37 = 37; //"="
+    int t_TOKEN_38 = 38; //"!="
+    int t_TOKEN_39 = 39; //"<"
+    int t_TOKEN_40 = 40; //"<="
+    int t_TOKEN_41 = 41; //">"
+    int t_TOKEN_42 = 42; //">="
+
+}
